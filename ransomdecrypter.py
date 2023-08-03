bf3cfac8dd7ae2b4999e650a44525e4f9e76001e
revolt36 <141328183+revolt36@users.noreply.github.com>
2023-08-03 22:27:48 +0400

    ransomdecrypter.py

--- ransomdecrypter.py
+++ ransomdecrypter.py
@@ -4,13 +4,13 @@ from cryptography.fernet import Fernet
 file_list = []
 
 for file in os.listdir():
-	if file == 'test.py' or file == 'generated.key' or file == 'desktop.ini' or file == 'ransomdecrypter.py':
+	if file == 'test.py' or file == 'generated.key' or file == 'ransomdecrypter.py':
 		continue
 	if os.path.isfile(file):
 		file_list.append(file)
 
-
-
+with open('generated.key', 'rb') as generatedkey:
+	secret_key = generatedkey.read()
 
 for file in file_list:
 	with open(file,'rb') as the_file:
